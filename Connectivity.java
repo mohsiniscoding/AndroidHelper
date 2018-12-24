c839ca235dd145bd7e9a13ccf4cf1365ebceca29
2 Ediotz <mohsin.doer@gmail.com>
2018-12-24 19:39:35 +0500

    Connectivity.java
    Network check class and connectivity helper

---
+++ Connectivity.java
@@ -0,0 +1,35 @@
+
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+public class Connectivity {
+
+    public static NetworkInfo getNetworkInfo(Context context){
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo();
+    }
+
+    public static boolean isConnected(Context context){
+        NetworkInfo info = Connectivity.getNetworkInfo(context);
+        return (info != null && info.isConnected());
+    }
+
+
+    public static boolean isConnectedWifi(Context context){
+        NetworkInfo info = Connectivity.getNetworkInfo(context);
+        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI);
+    }
+
+
+    public static boolean isConnectedMobile(Context context){
+        NetworkInfo info = Connectivity.getNetworkInfo(context);
+        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_MOBILE);
+    }
+
+
+
+
+}
